6054d8af06845950ce315660d0b96c1fdf68755f
geekcult <publicidade.exe@gmail.com>
2023-04-27 14:10:42 -0300

    Added Data Tranfer Objects

---
+++ src/resolvers/appointments_resolver.ts
@@ -0,0 +1,21 @@
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { CreateAppointmentInput } from "../dtos/inputs/create-appointment-input";
+import { AppointmentModel } from "../dtos/models/appointment-model";
+
+@Resolver()
+export class AppointmentsResolver{
+    @Query( () => [AppointmentModel])
+    async appointments () {
+        return []
+    };
+
+    @Mutation(() => AppointmentModel)
+    async createAppointment(@Arg('data') data: CreateAppointmentInput ){
+        const appointment = {
+            startsAt: data.startsAt,
+            endsAt: data.endsAt
+        }
+        return appointment
+    }
+
+}
